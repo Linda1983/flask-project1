edc53b9aadcac6d65c8205d4713c67f0a4beda91
Linda17071983 <linda.mtir@le-campus-numerique.fr>
2024-07-17 14:39:42 +0200

    importer Ollama

--- hello.py
+++ hello.py
@@ -19,12 +19,10 @@ response = ollama.chat(model='llama3', messages=[
 print(response['message']['content'])
 
 import ollama
-
-stream = ollama.chat(
-    model='llama3',
-    messages=[{'role': 'user', 'content': 'Why is the sky blue?'}],
-    stream=True,
-)
-
-for chunk in stream:
-  print(chunk['message']['content'], end='', flush=True)
+response = ollama.chat(model='llama3', messages=[
+  {
+    'role': 'user',
+    'content': 'Why is the sky blue?',
+  },
+])
+print(response['message']['content'])
